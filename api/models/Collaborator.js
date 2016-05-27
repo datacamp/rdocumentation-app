bdb360d06c847c1d4ae2a9138d406ec7b902eed0
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-05-27 09:21:37 +0200

    clean old model

--- api/models/Collaborator.js
+++ api/models/Collaborator.js
@@ -3,29 +3,7 @@
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
-   attributes: {
 
-    name : {
-      type: 'String',
-      required: true,
-      notNull: true
-    },
-
-    email : {
-      type: 'String',
-    },
-
-    maintainedPackages: {
-      collection: 'packageVersion',
-      via: 'maintainer'
-    },
-
-    authoredPackages: {
-      collection: 'packageVersion',
-      via: 'authors'
-    }
-
-  }
  */
 
 module.exports = {
@@ -42,7 +20,7 @@ module.exports = {
 
   },
   associations: function() {
-    Collaborator.belongsToMany(PackageVersion, {as: 'authors', through: 'Collaboration', foreignKey: 'author_id'});
+    Collaborator.belongsToMany(PackageVersion, {as: 'authors', through: 'Collaborations', foreignKey: 'author_id'});
     Collaborator.hasMany(PackageVersion, {as: 'maintained_packages', foreignKey: 'maintainer_id'});
   },
 
--- api/models/Dependency.js
+++ api/models/Dependency.js
@@ -3,36 +3,6 @@
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
- unique
-
- autoPK: false,
-
-  attributes: {
-    dependency: {
-      model: 'package',
-      required: true,
-      columnName: 'dependency_package_name'
-    },
-
-    dependant: {
-      model: 'packageversion',
-      required: true,
-      columnName: 'dependant_package_version'
-    },
-
-    dependencyVersion: {
-      model: 'String',
-      columnName: 'dependency_version',
-      required: false
-    },
-
-    comparator: {
-      type: 'String',
-      required: false,
-      enum: ['le', 'eq', 'ge', 'gt', 'lt']
-    }
-
-  }
  */
 
 module.exports = {
