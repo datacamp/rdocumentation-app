8d7c828acd323b6aa71caa9cdd3ca93b98d1e700
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-06 11:20:39 +0200

    increase pool size

--- config/env/staging.js
+++ config/env/staging.js
@@ -22,8 +22,8 @@ module.exports = {
         host   : process.env.DATABASE_HOST,
         port   : process.env.DATABASE_PORT,
         pool: {
-          max: 10,
-          min: 4,
+          max: 50,
+          min: 5,
           idle: 10000
         }
       }
