e3137ee26b326f122a1b798a94239dbc6c5a8346
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-06 11:34:08 +0200

    reduce pool size

--- config/env/staging.js
+++ config/env/staging.js
@@ -22,8 +22,8 @@ module.exports = {
         host   : process.env.DATABASE_HOST,
         port   : process.env.DATABASE_PORT,
         pool: {
-          max: 10,
-          min: 3,
+          max: 5,
+          min: 2,
           idle: 10000
         }
       }
