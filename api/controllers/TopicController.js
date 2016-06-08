f2fa022a0b3ed0b0c46374b46f4e83808367ab94
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-08 15:21:42 +0200

    show topic draft

--- api/controllers/TopicController.js
+++ api/controllers/TopicController.js
@@ -118,7 +118,7 @@ module.exports = {
       }]
     }).then(function(topic) {
       if(topic === null) return res.notFound();
-      else return res.ok(topic, 'topic/topic.ejs');
+      else return res.ok(topic.toJSON(), 'topic/topic.ejs');
     }).catch(function(err) {
       return res.negotiate(err);
     });
