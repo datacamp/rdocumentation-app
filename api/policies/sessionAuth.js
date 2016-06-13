15cb8f01ecb1dc8fcbff54014f07bcdd52547379
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-13 11:08:23 +0200

    push user to every views

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -12,6 +12,7 @@ module.exports = function(req, res, next) {
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
   if (req.isAuthenticated()) {
+    res.locals.user = req.user;
     return next();
   }
 
