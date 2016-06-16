85c3d245402f30f6e4b6062f7032278bd95e68c5
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-16 16:09:37 +0200

    elasticsearch hook

---
+++ api/hooks/es/index.js
@@ -0,0 +1,21 @@
+module.exports = function es_init(sails) {
+  global['Elasticsearch'] = require('elasticsearch');
+
+  return {
+
+    initialize: function(next) {
+
+      var config = sails.config[this.configKey];
+      var host = config.host;
+      if (host === null) {
+        throw new Error('Host not found in config/elasticsearch');
+      }
+
+      var es = new Elasticsearch.Client(config);
+      global['es'] = es;
+
+      next();
+    }
+
+  };
+};
---
+++ config/elasticsearch.js
@@ -0,0 +1,9 @@
+
+
+module.exports.elasticsearch = {
+
+  host: 'http://ec2-54-67-61-189.us-west-1.compute.amazonaws.com:9200',
+
+  log: 'trace'
+
+};
