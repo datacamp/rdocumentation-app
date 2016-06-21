9032059903728f4800487f3e044aa69cd733545b
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-21 10:51:55 +0200

    author cleaning task

---
+++ api/services/AuthorService.js
@@ -0,0 +1,46 @@
+var _ = require('lodash');
+var Promise = require('bluebird');
+
+module.exports = {
+
+  aggregateAuthors: function() {
+    var getDuplicatesAuthors = function () {
+      return sequelize.query("SELECT name FROM (select name, email, count(*) as sum from Collaborators group by name, email order by sum desc) s where s.sum > 1;", { type: sequelize.QueryTypes.SELECT});
+    };
+
+    return getDuplicatesAuthors().then(function(names) {
+      return Promise.map(names, function(nameInstance) {
+        var name = nameInstance.name;
+        return Collaborator.findAll({
+          where: {name: name},
+          order: [['email', 'DESC']],
+          include: [{
+            model: PackageVersion,
+            as: 'authored_packages',
+            attributes: ['id']
+          }]
+        }).then(function(collaborators) {
+          var stays = collaborators[0];
+          console.log(stays.toJSON());
+          var others = collaborators.slice(1);
+          var authored = _.reduce(others, function(acc, collaborator) {
+            return acc.concat(collaborator.authored_packages);
+          }, []);
+
+          var associate = stays.addAuthored_packages(authored);
+
+          var remove = Promise.map(others, function(other) {
+            return other.destroy();
+          });
+
+          return Promise.join(associate, remove, function(result, removeResult) {
+            return {name: name, result: 'success'};
+          });
+        });
+
+      });
+    });
+
+  }
+
+};
