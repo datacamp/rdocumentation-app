44f14952bf4ce615ce4eb96e8ccac53315df4094
Dieter De Mesmaeker <dieter@datacamp.com>
2016-06-22 12:27:03 +0200

    make generic toggle function

---
+++ assets/js/helpers/toggle.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  $('[data-toggle]').click(function(event){
+    event.preventDefault();
+    var target = $($(this).data('target')),
+    toggledText = $(this).data('toggled-text');
+
+    $(this).data('toggled-text', $(this).text());
+    $(this).text(toggledText);
+
+    if(target.data('toggle-hidden') === true){
+      target.show();
+      target.data('toggle-hidden', false);
+    } else {
+      target.hide();
+      target.data('toggle-hidden', true);
+    }
+  });
+});
