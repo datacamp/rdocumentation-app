a1abf5b6bebefbd565816ba9e83881edeb4443ce
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-22 12:40:07 +0200

    move generics to helpers

--- assets/js/helpers/list-table-filtering.js
+++ assets/js/helpers/list-table-filtering.js
@@ -32,16 +32,4 @@ $(document).ready(function() {
     }
   });
 
-  $('.download-task').each(function(elem) {
-    var $self = $(this);
-    var url = $self.data('url');
-    $.get(url, function(data){
-      console.log(data);
-      $self.find(".total").text(data.totalStr);
-      $self.find(".deps").text(data.revDepsStr);
-      $('.downloads').css({'visibility': 'visible'});
-    });
-  });
-
-
 });
