234374c474aa83a4da84ee2c9118bc481f06a49c
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-24 14:45:55 +0200

    fix stats table

--- migrations/sqls/20160624081405-download-statistics-up.sql
+++ migrations/sqls/20160624081405-download-statistics-up.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `DownloadStatistics` (
   `id` int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  `package_name` varchar(255) NOT NULL,
+  `package_name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `last_month_downloads` int(11) NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL,
   UNIQUE KEY `package` (`package_name`),
-  CONSTRAINT `DownloadStatistics_ibfk_1` FOREIGN KEY (`package_name`) REFERENCES `Packages` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB;
+  FOREIGN KEY (`package_name`) REFERENCES `Packages` (`name`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
