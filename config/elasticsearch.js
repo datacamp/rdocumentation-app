4825506b163f48157efca8fbd4ce2960068361b2
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-24 14:38:24 +0200

    change log level

--- config/elasticsearch.js
+++ config/elasticsearch.js
@@ -4,7 +4,7 @@ module.exports.elasticsearch = {
 
   host: 'http://ec2-54-67-61-189.us-west-1.compute.amazonaws.com:9200',
 
-  log: 'trace',
+  log: 'error',
 
   defer: function () {
     return bluebird.defer();
