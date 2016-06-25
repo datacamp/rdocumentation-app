3a247ef9180d0211143e63c626a9be35058f8de2
Dieter De Mesmaeker <dieter@datacamp.com>
2016-06-25 11:01:16 -0700

    fixes

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -1,3 +1,5 @@
+var axios = require('axios');
+
 module.exports = {
 
   me: function(req, res) {
@@ -21,23 +23,45 @@ module.exports = {
   },
 
   create: function(req, res) {
-    // Check if password and confirm_password are the same
-    if(req.body.password !== req.body.confirm_password){
-      FlashService.error(req, 'Passwords must match.');
-      return res.redirect('/register');
-    }
-    var result = User.create(req.body);
-    result.then(function(value) {
-      passport.authenticate('local', { successRedirect: '/',
-                                       failureRedirect: '/register',
-                                       failureFlash: true })(req, res);
-    }).catch(Sequelize.UniqueConstraintError, function (err) {
-      FlashService.error(req, 'Username "' + req.body.username + '" has been taken.');
-      return res.redirect('/register');
-    }).catch(Sequelize.ValidationError, function (err) {
-      err.errors.forEach(function(error){
-        FlashService.error(req, error.message);
-      });
+
+    // First check the recaptcha
+    var captchaResponse = req.body['g-recaptcha-response'];
+
+    axios.post('https://www.google.com/recaptcha/api/siteverify',{}, {
+      params: {
+        secret: process.env.RECAPTCHA_SECRET,
+        response: captchaResponse,
+        remoteip: req.ip
+      }
+    }).then(function(response){
+      if(response.data.success) {
+
+        // Check if password and confirm_password are the same
+        if(req.body.password !== req.body.confirm_password){
+          FlashService.error(req, 'Passwords must match.');
+          return res.redirect('/register');
+        }
+        var result = User.create(req.body);
+        result.then(function(value) {
+          passport.authenticate('local', { successRedirect: '/',
+                                           failureRedirect: '/register',
+                                           failureFlash: true })(req, res);
+        }).catch(Sequelize.UniqueConstraintError, function (err) {
+          FlashService.error(req, 'Username "' + req.body.username + '" has been taken.');
+          return res.redirect('/register');
+        }).catch(Sequelize.ValidationError, function (err) {
+          err.errors.forEach(function(error){
+            FlashService.error(req, error.message);
+          });
+          return res.redirect('/register');
+        });
+
+      } else {
+        FlashService.error(req, 'Cannot verify recaptcha');
+        return res.redirect('/register');
+      }
+    }).catch(function (error) {
+      FlashService.error(req, 'Cannot verify recaptcha');
       return res.redirect('/register');
     });
   }
