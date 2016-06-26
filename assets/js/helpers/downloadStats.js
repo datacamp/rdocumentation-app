0ecf0e3332e967956a83db85c3a967c2f423fef1
Dieter De Mesmaeker <dieter@datacamp.com>
2016-06-26 09:17:16 -0700

    fixes #7

--- assets/js/helpers/downloadStats.js
+++ assets/js/helpers/downloadStats.js
@@ -20,6 +20,22 @@ $(document).ready(function() {
     });
   });
 
+  $('.js-rating-download').each(function(elem) {
+    var $self = $(this);
+    var url = $self.data('url');
+    $.get(url, function(data){
+      var rounded = Math.round(data.rating*2)/2,
+          leftOver = rounded % 1,
+          index;
 
+      for (index = 0; index < rounded; index++) {
+        $self.find('.fa-star-o:nth-child(' + index + ')').removeClass('fa-star-o').addClass('fa-star');
+      }
+      if(leftOver == 0.5) {
+        $self.find('.fa-star-o:nth-child(' + index + ')').removeClass('fa-star-o').addClass('fa-star-half-o');
+      }
+      $self.text();
+    });
+  });
 
 });
