f5a39ff87ec4606ce4cef65e6864f8405cc15310
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-28 15:19:47 +0200

    Distinguish new relic app for staging and prod

--- newrelic.js
+++ newrelic.js
@@ -1,5 +1,5 @@
 exports.config = {
-  app_name: ['RDocs v2 Staging'],
+  app_name: [process.env.NEW_RELIC_APP],
   license_key: 'e6cd6d8afacfcc7191c34c933af26309dfa57cc6',
   logging: {
     level: 'warn', // can be error, warn, info, debug or trace
