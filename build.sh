c099cce9f79642040ec059d7ad3fc8ace9219b28
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-06-30 14:32:41 +0200

    build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [ -n "$(git status --porcelain)" ]; then
+  echo "Your git directory is not clean, commit your changes before building";
+  exit 1;
+fi
+
+#bump version
+$version=$(npm version $1)
+
+#build new docker image
+docker build -t dockerhub.datacamp.com/rdocsv2:$version .
+#push image to docker registery
+docker push dockerhub.datacamp.com/rdocsv2:$version
+
+#push to git
+git push --follow-tags
+
+sed "s/\$version/$version/" < Dockerrun.aws.json.in > Dockerrun.aws.json
