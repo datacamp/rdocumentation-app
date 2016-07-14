4dcd499fbce328067222e367b7d373bdf39658d1
Ludovic Vannoorenberghe <ludo@datacamp.com>
2016-07-14 13:20:28 +0200

    Use TEXT instead of varchar for all columns of PackageVersions

---
+++ migrations/sqls/20160714100838-stringToText-up.sql
@@ -0,0 +1,5 @@
+/* Replace with your SQL commands */
+
+ALTER TABLE `PackageVersions` MODIFY `license` TEXT DEFAULT NULL;
+ALTER TABLE `PackageVersions` MODIFY `title` TEXT DEFAULT NULL;
+ALTER TABLE `PackageVersions` MODIFY `copyright` TEXT DEFAULT NULL;
