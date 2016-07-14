c94680a4415ecdc6ffed0deb7c2f7466ac22622c
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-07-14 09:15:08 +0200

    Revert "updated models and migrations with date"
    This reverts commit 50f4f9fb50436124f9b3f7b0a01f3ab3429fefe8.

--- migrations/sqls/20160711142622-splitted-downloads-up.sql
+++ migrations/sqls/20160711142622-splitted-downloads-up.sql
@@ -1,5 +1,4 @@
 /* Replace with your SQL commands */
 ALTER TABLE DownloadStatistics
 ADD (`last_month_downloads_direct` int(11) NOT NULL,
-     `last_month_downloads_indirect` int(11) NOT NULL,
-     `date` DATE NOT NULL,);
+     `last_month_downloads_indirect` int(11) NOT NULL);
