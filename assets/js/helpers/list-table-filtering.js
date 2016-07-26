50d076bd1fde42ab61607b59f18df1b89e8dd14b
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-07-26 12:49:20 +0200

    added package filtering and topics

--- assets/js/helpers/list-table-filtering.js
+++ assets/js/helpers/list-table-filtering.js
@@ -24,6 +24,30 @@ $(document).ready(function() {
       return false;
     });
 
+
+    if(filteredRows.length === 0) {
+      $('.no-results').show();
+    } else {
+      $('.no-results').hide();
+      filteredRows.show();
+    }
+  });
+  $('#packagefilter').keyup(function () {
+    var indexColumn = 1, // Search for values in the second  column
+    searchWords = this.value.split(" "),
+    rows = $("#filterableItems").find("tr");
+
+    rows.hide();
+    //Recusively filter the jquery object to get results.
+    var filteredRows = rows.filter(function (i, v) {
+      var $t = $(this).children(":eq("+indexColumn+")");
+      for (var d = 0; d < searchWords.length; ++d) {
+        if ($t.is(":containsRaw('" + searchWords[d].toLowerCase() + "')")) {
+          return true;
+        }
+      }
+      return false;
+    });
     if(filteredRows.length === 0) {
       $('.no-results').show();
     } else {
