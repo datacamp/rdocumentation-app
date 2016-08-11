de5b5881e659a665050dd64f0240017ccad74c74
Hannes buseyne <hannes.buseyne@student.kuleuven.be>
2016-08-11 17:51:32 +0200

    solved dependency link after versioncheck

--- assets/js/pages/package.js
+++ assets/js/pages/package.js
@@ -2,7 +2,7 @@ window.packageVersionToggleHandler = function() {
   $('#packageVersionSelect').change(function(){
     var url = $(this).find('option:selected').data('uri');
     if(urlParam('viewer_pane') === '1'){
-      window.replacePage(url,false);
+      window.replacePage(url,true);
     } else {
       window.location.href = url;
     }
