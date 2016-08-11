85f9c0249f65d53178d252eea82467f68b449d87
louis <delanoeyelouis@gmail.com>
2016-08-11 15:55:28 +0200

    first rough version

--- assets/js/pages/task_views.js
+++ assets/js/pages/task_views.js
@@ -3,4 +3,21 @@ $(document).ready(function() {
     e.preventDefault();
     $(this).parent().find(".js-view-package-list").toggle();
   });
+  $(".list-group-item").each(function(){
+  	$(this).click(function(event){
+  		event.preventDefault();
+  		getView($(this).html());
+  		$(".highlight").removeClass("highlight");
+  		$(this).addClass("highlight");
+  	});
+  });
 });
+
+getView = function(view){
+  $.ajax({
+  	url: "/taskviews/"+view
+  }).done(function(response){
+  	$(".view").html($($.parseHTML(response)).filter(".content").html());
+  	resetFilter();
+  });
+}
