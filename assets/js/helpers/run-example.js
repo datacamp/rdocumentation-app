7a88b3532955b5bc5c32fae79eb490b242a4a14a
louis <delanoeyelouis@gmail.com>
2016-08-11 10:10:08 +0200

    added datacamp light

---
+++ assets/js/helpers/run-example.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+	$('.exampleR').attr("data-type","sample-code");
+	$('.exampleR').wrap("<div class = 'substitute'></div>").parent().prepend("<code class='nohighlight' data-type='pre-exercise-code'>library("+$(".exampleR").attr("data-package-name")+")</code>");
+	var substitution = $('.substitute').html();
+	console.log(substitution);
+	$('.substitute').replaceWith("<div data-datacamp-exercise data-lang='r'>"+substitution+"</div>");
+});
