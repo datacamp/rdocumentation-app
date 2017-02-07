f71a2d4593bc018c5b600e1d5d8990c5f8586809
Ludovic Vannoorenberghe <ludo@datacamp.com>
2017-02-07 18:00:58 +0100

    add chai and test

--- test/test-version-ordering.js
+++ test/test-version-ordering.js
@@ -1,5 +1,6 @@
 const lifter = require('../jake/sails-lifter');
 const _ = require('lodash');
+const should = require('chai').should()
 
 describe('Version ordering', function() {
   this.timeout(30000);
@@ -12,7 +13,8 @@ describe('Version ordering', function() {
 
   it('should order versions properly', (done) => {
     var versions = ['0.43-8', '1.4.1.1', '1.10', '1.2a.1', '1.1-0'];
-    console.log(versions.sort(PackageService.compareVersions('desc')));
+    var expected = [ '1.10', '1.4.1.1', '1.2a.1', '1.1-0', '0.43-8' ];
+    versions.should.deep.equal(expected);
     done()
   })
 })
