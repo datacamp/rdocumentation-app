25b61a8cf7ed0c409f05e0e145cd0694da3eb3e6
Jesse Hoobergs <jesse@hoobergs.be>
2017-06-27 15:07:29 +0200

    Use databaseconfig from .env file in docker

--- config/env/docker.js
+++ config/env/docker.js
@@ -18,13 +18,13 @@ module.exports = {
    ***************************************************************************/
    connections: {
     sequelize_mysql: {
-      database: 'rdoc',
-      user: 'root',
-      password: 'password',
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
       options: {
         dialect: 'mysql',
-        host   : 'db',
-        port   : 3306,
+        host   : process.env.DATABASE_HOST,
+        port   : process.env.DATABASE_PORT,
         pool: {
           max: 10,
           min: 4,
