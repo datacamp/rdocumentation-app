c0a5d4fc55a37615125bdc3f092b20caf49054b8
Ludovic Vannoorenberghe <ludo@datacamp.com>
2018-04-04 14:33:01 +0200

    fix PackageVersionService

--- api/services/PackageVersionService.js
+++ api/services/PackageVersionService.js
@@ -1,67 +1,68 @@
-module.exports = {
-  getSourceList: function(res, package_name, version){
-    var key = 'rdocs_source_' + package_name + '_' + version + '_tree';
-    return RedisService.getJSONFromCache(key, res, RedisService.DAILY, function() {
-      var prefix = "rpackages/unarchived/" + package_name + "/" + version + "/R/";
-      return s3Service.getAllFilesInFolder(prefix, true)
-        .then(function(data){
-          var url = process.env.BASE_URL + "/packages/" + package_name + "/versions/" + version
-              + "/source/";
+function getSourceList(res, package_name, version) {
+  var key = 'rdocs_source_' + package_name + '_' + version + '_tree';
+  return RedisService.getJSONFromCache(key, res, RedisService.DAILY, function () {
+    var prefix = "rpackages/unarchived/" + package_name + "/" + version + "/R/";
+    return s3Service.getAllFilesInFolder(prefix, true)
+      .then(function (data) {
+        var url = process.env.BASE_URL + "/packages/" + package_name + "/versions/" + version
+          + "/source/";
 
-          var list = data.list.map(function(item){
-            var name = item.Key.substring(prefix.length, item.Key.length);
-            var parts = name.split('/');
-            return {
-              'name': name,
-              'parts': parts
-            }
-          });
-          var data = {};
-          for(var item of list){
-            PackageVersionService.setObjectValue(data, item.parts, item.name);
-          }
-          var tree = [];
-          PackageVersionService.toTreeStructure(tree, data);
+        var list = data.list.map(function (item) {
+          var name = item.Key.substring(prefix.length, item.Key.length);
+          var parts = name.split('/');
           return {
-              tree: tree
+            'name': name,
+            'parts': parts
           }
         });
-
-    });
-  },
-
-  setObjectValue: function(obj, indices, value) {
-    if (indices.length==1)
-        return obj[indices[0]] = value;
-    else if (indices.length==0)
-        return obj;
-    else{
-        if(obj[indices[0]] === undefined)
-          obj[indices[0]] = {};
-        return setObjectValue(obj[indices[0]],indices.slice(1), value);
-    }
-  },
-
-  toTreeStructure: function(tree, data){
-    if(typeof(data) !== 'object')
-      return;
-    for(var key of Object.keys(data)){
-      var nodes = [];
-      PackageVersionService.toTreeStructure(nodes, data[key]);
-      var node = {
-        text: key,
-        selectable: nodes.length === 0,
-        state: {
-          selected: false
+        var data = {};
+        for (var item of list) {
+          setObjectValue(data, item.parts, item.name);
         }
-      };
-      if(nodes.length === 0)
-        node.href = data[key];
-      else
-        node.nodes = nodes;
-      tree.push(node);
-    }
+        var tree = [];
+        toTreeStructure(tree, data);
+        return {
+          tree: tree
+        }
+      });
+
+  });
+}
+
+function setObjectValue(obj, indices, value) {
+  if (indices.length == 1)
+    return obj[indices[0]] = value;
+  else if (indices.length == 0)
+    return obj;
+  else {
+    if (obj[indices[0]] === undefined)
+      obj[indices[0]] = {};
+    return setObjectValue(obj[indices[0]], indices.slice(1), value);
   }
+}
 
-
+function toTreeStructure(tree, data) {
+  if (typeof (data) !== 'object')
+    return;
+  for (var key of Object.keys(data)) {
+    var nodes = [];
+    toTreeStructure(nodes, data[key]);
+    var node = {
+      text: key,
+      selectable: nodes.length === 0,
+      state: {
+        selected: false
+      }
+    };
+    if (nodes.length === 0)
+      node.href = data[key];
+    else
+      node.nodes = nodes;
+    tree.push(node);
+  }
+}
+module.exports = {
+  getSourceList,
+  setObjectValue,
+  toTreeStructure
 }
