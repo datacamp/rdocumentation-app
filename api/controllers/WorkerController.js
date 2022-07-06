b5f68fef5bd5d333d7acd8e56fb152c6d06cfb32
ben-brooker <ben.brooker@datacamp.com>
2022-07-06 16:57:51 +0100

    chore: check api deploy message ecs

--- api/controllers/WorkerController.js
+++ api/controllers/WorkerController.js
@@ -25,10 +25,10 @@ module.exports = {
         res.json(value);
       })
       .catch(Sequelize.UniqueConstraintError, function (err) {
-        console.log('SequelizeError: UniqueConstraintError: ', err.errors);
+        console.log('SequelizeError - UniqueConstraintError: ', err.errors);
         return res.send(409, err.errors);
       }).catch(Sequelize.ValidationError, function (err) {
-        console.log('SequelizeError: ValidationError: ', err.errors);
+        console.log('SequelizeError - ValidationError: ', err.errors);
         return res.send(400, err.errors);
       }).catch(function(err){
         return res.negotiate(err.errors);
